6cb43ec066f4d9943c7f43de2072010e51aa9de7
Ubuntu <mikaelatang@tee.xooy3fmlnqguzdlgzx12hdst4g.bx.internal.cloudapp.net>
2024-04-26 15:15:52 +0000

    add gsc config.yaml

--- gatk-validation/gatk.sh
+++ gatk-validation/gatk.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 whereis python
-/gatk/gatk --list
-/gatk/gatk ValidateSameFile -I /inputs/NA12878_24RG_small.hg38.bam -MODE SUMMARY
+cd gatk
+gatk --list
+gatk ValidateSameFile -I /inputs/NA12878_24RG_small.hg38.bam -MODE SUMMARY
