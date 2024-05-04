0fb35687ee9cb2d625aecc647523385bbbddca2d
Ubuntu <azureuser@tee2.xooy3fmlnqguzdlgzx12hdst4g.bx.internal.cloudapp.net>
2024-05-04 23:46:30 +0000

    working version of gatk-validation

--- gatk-validation/entrypoint.sh
+++ gatk-validation/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-gramine-sgx-get-token --output bash.token --sig bash.sig
-gramine-sgx ./bash -c "pwd && ls && bash /app/gatk.sh"
-#gramine-sgx gatk ValidateSameFile -I /inputs/NA12878_24RG_small.hg38.bam -M SUMMARY 
+gramine-sgx ./bash -c "bash /app/gatk.sh"
--- gatk-validation/gatk.sh
+++ gatk-validation/gatk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-whereis python
 cd gatk
-gatk --list
-gatk ValidateSameFile -I /inputs/NA12878_24RG_small.hg38.bam -MODE SUMMARY
+ulimit -n 65000
+./gatk --list
+./gatk ValidateSamFile -I /inputs/NA12878_24RG_small.hg38.bam -M SUMMARY
